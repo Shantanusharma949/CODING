d26acbb3f69ccb284feee26e717a3c6e78850373
Shantanu Sharma <shansharma1301@gmail.com>
2021-05-10 20:49:04 +0530

    Add files via upload

---
+++ CIRCULAR LINKED LIST.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<conio.h>
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+struct node *head,*temp,*newnode;
+
+void create(int x)
+{
+  newnode=(struct node*)malloc(sizeof(struct node));
+  newnode->data=x;
+  newnode->link=NULL;
+
+  if(head==NULL)
+        head=temp=newnode;
+  else
+  {
+      temp->link=newnode;
+      temp=newnode;
+      newnode->link=head;
+  }
+}
+
+void display()
+{
+    temp=head;
+    printf("Circular Linked List is: %d\n",temp->data);
+    temp=temp->link;
+    while(temp!=head)
+    {
+        printf("%d\n",temp->data);
+        temp=temp->link;
+    }
+}
+//TO CHECK IT IS A CIRCULAR LINKED LIST OR NOT//
+void display11()
+{
+    int c=1;
+    temp=head;
+    printf("Circular Linked List is: %d\n",temp->data);
+    temp=temp->link;
+    while(c!=0)
+    {
+        if(temp==head)
+            c=0;
+        printf("%d\n",temp->data);
+        temp=temp->link;
+
+    }
+}
+
+void main()
+{
+    create(1);
+    create(8);
+    create(3);
+    create(7);
+    create(11);
+    create(22);
+
+    display();
+    display11();
+}
